a4debbc066b9c972c8a212360652fd90f8cd3d90
heejung <dap072055@naver.com>
2024-09-13 11:46:01 +0900

    :sparkles: feat: member-service

---
+++ src/main/java/com/example/demo/member/service/MemberService.java
@@ -0,0 +1,9 @@
+package com.example.demo.member.service;
+
+import com.example.demo.member.dto.request.MemberCreate;
+
+//메소드 - 기능
+public interface MemberService {
+
+  void createMember(MemberCreate memberCreate);
+}
---
+++ src/main/java/com/example/demo/member/service/MemberServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.member.service;
+
+import com.example.demo.member.dto.request.MemberCreate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+  @Override
+  public void createMember(MemberCreate memberCreate) {
+
+  }
+}
